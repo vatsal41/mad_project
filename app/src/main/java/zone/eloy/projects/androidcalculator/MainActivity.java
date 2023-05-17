cfa34988e5fc9b9c9680f4396027091cd850565d
vatsal41 <211b341@juetguna.in>
2023-05-17 23:53:34 +0530

    minor fixes

--- app/src/main/java/zone/eloy/projects/androidcalculator/MainActivity.java
+++ app/src/main/java/zone/eloy/projects/androidcalculator/MainActivity.java
@@ -91,3 +91,119 @@ TextView textViewInputNumbers;
         buttonDot = (Button) findViewById(R.id.button_dot);
         textViewInputNumbers = (TextView) findViewById(R.id.textView_input_numbers);
     }
+private void setOnClickListeners()
+    {
+        buttonNumber0.setOnClickListener(this);
+        buttonNumber1.setOnClickListener(this);
+        buttonNumber2.setOnClickListener(this);
+        buttonNumber3.setOnClickListener(this);
+        buttonNumber4.setOnClickListener(this);
+        buttonNumber5.setOnClickListener(this);
+        buttonNumber6.setOnClickListener(this);
+        buttonNumber7.setOnClickListener(this);
+        buttonNumber8.setOnClickListener(this);
+        buttonNumber9.setOnClickListener(this);
+
+        buttonClear.setOnClickListener(this);
+        buttonParentheses.setOnClickListener(this);
+        buttonPercent.setOnClickListener(this);
+        buttonDivision.setOnClickListener(this);
+        buttonMultiplication.setOnClickListener(this);
+        buttonSubtraction.setOnClickListener(this);
+        buttonAddition.setOnClickListener(this);
+        buttonEqual.setOnClickListener(this);
+        buttonDot.setOnClickListener(this);
+    }
+
+    private void setOnTouchListener()
+    {
+        buttonNumber0.setOnTouchListener(this);
+        buttonNumber1.setOnTouchListener(this);
+        buttonNumber2.setOnTouchListener(this);
+        buttonNumber3.setOnTouchListener(this);
+        buttonNumber4.setOnTouchListener(this);
+        buttonNumber5.setOnTouchListener(this);
+        buttonNumber6.setOnTouchListener(this);
+        buttonNumber7.setOnTouchListener(this);
+        buttonNumber8.setOnTouchListener(this);
+        buttonNumber9.setOnTouchListener(this);
+
+        buttonClear.setOnTouchListener(this);
+        buttonParentheses.setOnTouchListener(this);
+        buttonPercent.setOnTouchListener(this);
+        buttonDivision.setOnTouchListener(this);
+        buttonMultiplication.setOnTouchListener(this);
+        buttonSubtraction.setOnTouchListener(this);
+        buttonAddition.setOnTouchListener(this);
+        buttonDot.setOnTouchListener(this);
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        switch (view.getId())
+        {
+            case R.id.button_zero:
+                if (addNumber("0")) equalClicked = false;
+                break;
+            case R.id.button_one:
+                if (addNumber("1")) equalClicked = false;
+                break;
+            case R.id.button_two:
+                if (addNumber("2")) equalClicked = false;
+                break;
+            case R.id.button_three:
+                if (addNumber("3")) equalClicked = false;
+                break;
+            case R.id.button_four:
+                if (addNumber("4")) equalClicked = false;
+                break;
+            case R.id.button_five:
+                if (addNumber("5")) equalClicked = false;
+                break;
+            case R.id.button_six:
+                if (addNumber("6")) equalClicked = false;
+                break;
+            case R.id.button_seven:
+                if (addNumber("7")) equalClicked = false;
+                break;
+            case R.id.button_eight:
+                if (addNumber("8")) equalClicked = false;
+                break;
+            case R.id.button_nine:
+                if (addNumber("9")) equalClicked = false;
+                break;
+            case R.id.button_addition:
+                if (addOperand("+")) equalClicked = false;
+                break;
+            case R.id.button_subtraction:
+                if (addOperand("-")) equalClicked = false;
+                break;
+            case R.id.button_multiplication:
+                if (addOperand("x")) equalClicked = false;
+                break;
+            case R.id.button_division:
+                if (addOperand("\u00F7")) equalClicked = false;
+                break;
+            case R.id.button_percent:
+                if (addOperand("%")) equalClicked = false;
+                break;
+            case R.id.button_dot:
+                if (addDot()) equalClicked = false;
+                break;
+            case R.id.button_parentheses:
+                if (addParenthesis()) equalClicked = false;
+                break;
+            case R.id.button_clear:
+                textViewInputNumbers.setText("");
+                openParenthesis = 0;
+                dotUsed = false;
+                equalClicked = false;
+                break;
+            case R.id.button_equal:
+                if (textViewInputNumbers.getText().toString() != null && !textViewInputNumbers.getText().toString().equals(""))
+                    calculate(textViewInputNumbers.getText().toString());
+                break;
+        }
+
+    }
