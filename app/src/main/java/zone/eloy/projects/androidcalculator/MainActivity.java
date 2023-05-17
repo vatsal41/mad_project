ad1df065484f9041f9b0634fce5363132a4f8319
vatsal41 <211b341@juetguna.in>
2023-05-17 23:28:57 +0530

    added packages

--- app/src/main/java/zone/eloy/projects/androidcalculator/MainActivity.java
+++ app/src/main/java/zone/eloy/projects/androidcalculator/MainActivity.java
@@ -1,14 +1,17 @@
-package com.example.myapplication;
+package zone.eloy.projects.androidcalculator;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.math.BigDecimal;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-}
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
